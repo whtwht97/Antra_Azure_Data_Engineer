ae22a412c793a0e5dda28fa3704ae120d9016460
Haotian Wu <60466947+whtwht97@users.noreply.github.com>
2022-11-30 07:04:18 -0800

    Add files via upload

---
+++ Databrick/includes/configuration.py
@@ -0,0 +1,43 @@
+# Databricks notebook source
+# MAGIC 
+# MAGIC %md
+# MAGIC Define Data Paths.
+
+# COMMAND ----------
+
+# ANSWER
+username = "haotianwu"
+
+# COMMAND ----------
+
+peopleDimPath = f"/dbacademy/{username}/dataengineering/people/"
+classicPipelinePath = f"/dbacademy/{username}/dataengineering/classic/"
+
+landingPath = classicPipelinePath + "landing/"
+rawPath = classicPipelinePath + "raw/"
+bronzePath = classicPipelinePath + "bronze/"
+silverPath = classicPipelinePath + "silver/"
+silverQuarantinePath = classicPipelinePath + "silverQuarantine/"
+goldPath = classicPipelinePath + "gold/"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Configure Database
+
+# COMMAND ----------
+
+spark.sql(f"CREATE DATABASE IF NOT EXISTS dbacademy_{username}")
+spark.sql(f"USE dbacademy_{username}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Import Utility Functions
+
+# COMMAND ----------
+
+# MAGIC %run ./utilities
+
+# COMMAND ----------
+
